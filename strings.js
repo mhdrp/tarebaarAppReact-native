3257d61f288c6460bb89d65ac000bb783d58be93
yektateam <mehradramezanipour@gmail.com>
2019-04-12 20:17:10 +0430

    refactor/strings

--- strings.js
+++ strings.js
@@ -1,3 +1,50 @@
 export default {
+    color: {
+        bgColor: "#f9f9fb",
+        txtPlaceInput: "#adb3bf",
+        txtInput:"#434b55",
+        txtBtn: "#ffffff",
 
+        title: "#404852",
+        text: "#afb5c0",
+
+        primaryBtn: "#535bfe",
+    },
+
+    msg: {
+        login: {
+            loginOrRegister: "ورورد / عضویت",
+            login: "ورود",
+            phoneNumber: "شماره موبایل",
+        },
+
+    },
+
+    size: {
+        /* size border radius */
+        borderRadius: 25,
+        normalBorderRadius: 15,
+
+        /* size title */
+        largeTitle: 32,
+        normalTitle: 28,
+        smallTitle: 24,
+        smallerTitle: 16,
+        /* size text */
+        largerText: 18,
+        largeText: 16,
+        normalText: 14,
+        smallText: 12,
+        smallerText: 10,
+        smallestText: 9,
+
+        /* size button */
+        largeBtn: 22,
+        normalBtn: 18,
+        smallBtn: 16,
+    },
+    font: {
+        medium: "IRANSansWeb Medium",
+        light: "IRANSansWeb Light",
+    },
 }
