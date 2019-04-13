541cecd30acaf7fb41183aa3ef4aed477522eecd
yektateam <mehradramezanipour@gmail.com>
2019-04-13 18:06:28 +0430

    add class primitive Verify-page

---
+++ login/Verify.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import {I18nManager, TextInput, View} from 'react-native';
+
+I18nManager.forceRTL(true);
+
+export default class Verify extends Component {
+
+    render() {
+
+        return (
+            <View>
+
+            </View>
+        );
+    }
+} 
