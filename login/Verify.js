c95166b0932a63b301a0992b8b4e40ebce76041b
yektateam <mehradramezanipour@gmail.com>
2019-04-13 18:19:48 +0430

    feature/countdown resend sms in verify-page

--- login/Verify.js
+++ login/Verify.js
@@ -7,8 +7,61 @@ import {I18nManager, TextInput, View} from 'react-native';
 I18nManager.forceRTL(true);
 
 export default class Verify extends Component {
+    constructor() {
+        super();
+        this.state = {checkTime: true, time: {}, seconds: 90};
+        this.timer = 0;
+        this.startTimer = this.startTimer.bind(this);
+        this.countDown = this.countDown.bind(this);
+    }
+
+    static secondsToTime(secs) {
+        let hours = Math.floor(secs / (60 * 60));
+        let divisor_for_minutes = secs % (60 * 60);
+        let minutes = Math.floor(divisor_for_minutes / 60);
+
+        let divisor_for_seconds = divisor_for_minutes % 60;
+        let seconds = Math.ceil(divisor_for_seconds);
+
+        let obj = {
+            "h": hours,
+            "m": minutes,
+            "s": seconds
+        };
+        return obj;
+    }
+
+
+    componentDidMount() {
+        let timeLeftVar = Verify.secondsToTime(this.state.seconds);
+        this.setState({time: timeLeftVar});
+    }
+
+    startTimer() {
+        if (this.timer === 0 && this.state.seconds > 0) {
+            this.timer = setInterval(this.countDown, 1000);
+
+        }
+    }
+
+    countDown() {
+        // Remove one second, set state so a re-render happens.
+        let seconds = this.state.seconds - 1;
+        this.setState({
+            time: Verify.secondsToTime(seconds),
+            seconds: seconds,
+        });
+
+        // Check if we're at zero.
+        if (seconds === 0) {
+            clearInterval(this.timer);
+            this.timer = 0;
+            this.setState({checkTime: false});
+        }
+    }
 
     render() {
+        this.startTimer();
 
         return (
 
@@ -61,15 +114,20 @@ export default class Verify extends Component {
                     {/*button for resend sms*/}
                     <Button
                         block
-                        style={[styles.btnPrimary, styles.btnColorPrimary]}>
+                        style={[styles.btnPrimary, styles.btnColorPrimary]}
+                        onPress={() => this.props.navigation.navigate('Index')}>
                         <Text style={styles.txtBtn}>{strings.msg.verification}</Text>
                     </Button>
 
                     {/*button for resend sms*/}
-                    <Button block transparent>
-                        <Text>
-                            {strings.msg.resendCode}
+                    <Button block transparent disabled={this.state.checkTime}
 
+                            onPress={() => {
+                                this.setState(prevState => ({checkTime: true, seconds: 90,}));
+                            }}>
+                        <Text style={this.state.checkTime ? styles.verifyResendDisable : styles.verifyResendEnable}>
+                            {strings.msg.resendCode}
+                            ({this.state.time.m}:{this.state.time.s})
                         </Text>
                     </Button>
 
