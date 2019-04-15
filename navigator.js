9d4dea6cb7e789669e2d66ddc4ac119e1d12dca6
yektateam <mehradramezanipour@gmail.com>
2019-04-15 11:38:43 +0430

    add main-page to navigator

--- navigator.js
+++ navigator.js
@@ -1,12 +1,13 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'; // Version can be specified in package.json
 import SignIn from './login/SingIn';
 import Verify from './login/Verify';
-
+import Main from './view/main';
 
 const RootStack = createStackNavigator(
     {
         SignIn: SignIn,
         Verify: Verify,
+        Main: Main,
     },
     {
         initialRouteName: 'SignIn',
